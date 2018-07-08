2c4253ad1f805cbcf231262d21d5294dd4d0810e
Tommy Martin <>
2018-07-07 23:01:28 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  },
+  data () {
+    return {
+      title: 'my first vue app, woo!'
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
